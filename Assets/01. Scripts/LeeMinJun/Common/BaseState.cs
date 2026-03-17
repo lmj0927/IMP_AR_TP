abcacf63221c146b5d34935bd15f3ea2b2f0494b
MinJunLee <lmjj0927@naver.com>
2026-03-17 11:38:32 +0900

    Common Scripts

---
+++ Assets/01. Scripts/LeeMinJun/Common/BaseState.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public abstract class BaseState<T> : IState
+{
+    protected T _controller;
+
+    public BaseState(T controller)
+    {
+        _controller = controller;
+    }
+
+    public abstract void Enter();
+    public abstract void UpdateState();
+    public abstract void Exit();
+}
+
+public interface IState
+{
+    public void Enter();
+    public void UpdateState();
+    public void Exit();
+}
